a41a5f70ac198921b457fd7660895c64574a2114
priyanka vaidya <vaidyapriyanka80@gmail.com>
2020-09-09 20:20:48 +0530

    python project

---
+++ web/webapp/forms.py
@@ -0,0 +1,49 @@
+from django  import forms
+from .models import Post,Category,Comment
+
+
+#choices = [('coding', 'coding'),('sports', 'sports'),('entertainment', 'entertainment')]
+choices = Category.objects.all().values_list('name','name')
+
+choice_list = []
+
+for item in choices:
+    choice_list.append(item)
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = {'title','title_tag','author','category','body','snippet','header_image'}
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class' : 'form-control','placeholder' : 'Write the title of Post'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control','placeholder' : 'Write the title tag here.'}),
+            'author': forms.TextInput(attrs={'class': 'form-control','value' : '', 'id':'author-id', 'type':'hidden'}),
+            #'author': forms.Select(attrs={'class': 'form-control'}),
+            'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'snippet':forms.Textarea(attrs={'class': 'form-control'}),
+        }
+
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = {'title','title_tag','body','snippet'}
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class' : 'form-control','placeholder' : 'Write the title of Post'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control','placeholder' : 'Write the title tag here.'}),
+            #'author': forms.Select(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'snippet': forms.Textarea(attrs={'class': 'form-control'}),
+        }
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name','body')
+
+        widgets = {
+            'name': forms.TextInput(attrs={'class' : 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+        }
